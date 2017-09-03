f7a0506983f752de4d44307e3c99b9494df46752
benjaporn01 <59030179@kmitl.ac.th>
2017-09-03 11:41:20 +0700

    add byte

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,18 +6,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            string name, lname, ID, GPA;
-            Console.Write("Enter name:");
-            name = Console.ReadLine();
-            Console.Write("Enter Lastname:");
-            lname = Console.ReadLine();
-            Console.Write("Enter ID:");
-            ID = Console.ReadLine();
-            Console.Write("Enter GPA.:");
-            GPA = Console.ReadLine();
-            Console.WriteLine("------------------");
-            Console.Write("Name: {0}\nLastname: {1}\nID: {2}\nGPA.:{3}\n",name,lname,ID,GPA);
-            Console.ReadKey();
+            Console.WriteLine("Data type : byte");
+            Console.WriteLine("Size :" + sizeof(byte));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
     }
 }
