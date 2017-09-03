1504be822608e7216c20fe3999e479598651da16
benjaporn01 <59030179@kmitl.ac.th>
2017-09-03 11:47:37 +0700

    add size short

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,8 +6,8 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : sbyte");
-            Console.WriteLine("Size :" + sizeof(sbyte));
+            Console.WriteLine("Data type : short");
+            Console.WriteLine("Size :" + sizeof(short));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);  
         }
