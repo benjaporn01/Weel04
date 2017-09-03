8e0c7511e97944407d39a0acb9e5afd8edc8dc50
benjaporn01 <59030179@kmitl.ac.th>
2017-09-03 11:48:37 +0700

    add size ushort

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,8 +6,8 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : short");
-            Console.WriteLine("Size :" + sizeof(short));
+            Console.WriteLine("Data type : ushort");
+            Console.WriteLine("Size :" + sizeof(ushort));
             Console.WriteLine("Minimum Value :" + int.MinValue);
             Console.WriteLine("Maximum Value :" + int.MaxValue);  
         }
