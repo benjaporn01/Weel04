43a3ae1756f5f70e3be588fc58e772efacd77b5e
benjaporn01 <59030179@kmitl.ac.th>
2017-09-03 12:35:50 +0700

    random program

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,14 @@ namespace RandomNumber
         static void Main(string[] args)
         {
             Random random = new Random();
-            int randomNumber = random.Next(0, 100);
-            Console.WriteLine(randomNumber);
+            int a,randomNumber = random.Next(0, 9);
+            bool b;
+            Console.Write("Enter nember:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Random number:{0}",randomNumber);
+            b = a > randomNumber;
+            Console.WriteLine("Boolean {0} > {1} is {2}",a,randomNumber,b);
+            Console.ReadKey();
         }
     }
 }
